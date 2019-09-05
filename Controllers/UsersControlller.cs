ec25ec1a557b607c400a0d289c9c7681a236259e
vince-sparks <vince.sparks@hpe.com>
2019-09-05 15:24:40 -0500

    initial commit

---
+++ Controllers/UsersControlller.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using JwtService.Entities;
+using JwtService.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JwtService.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+        private readonly ILogger _logger;
+
+        public UsersController(IUserService userService, ILogger<UsersController> logger)
+        {
+            _userService = userService;
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            Console.WriteLine("test");
+
+            return Ok();
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            //Console.WriteLine("authenticate");
+            _logger.LogInformation("authenticate");
+
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            _logger.LogInformation("getall");
+            var users =  _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace JwtService.Helpers 
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
